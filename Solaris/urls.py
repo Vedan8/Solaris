a7232e14dbe5d6e247c43dbebb548161d8c1c0fa
Vedan8 <vedanjyadav@gmail.com>
2024-12-07 00:40:12 +0530

    fix

--- Solaris/urls.py
+++ Solaris/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from .views import GoogleLogin
+from django.urls import path,includ
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('SunLocation.urls')),
-    path('api/', include('email_auth.urls')),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('api/', include('email_auth.urls'))
 ]
