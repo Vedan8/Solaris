1846a1f5f2d58b8b3c8732c9186f6b7dd0419a88
Vedan8 <131740640+Vedan8@users.noreply.github.com>
2024-12-12 16:15:59 +0530

    face

--- SunLocation/views.py
+++ SunLocation/views.py
@@ -199,12 +199,35 @@ class SolarPotentialEachFaceView(APIView):
         hourly_potential = {"face1": [], "face2": [], "face3": [], "face4": []}
 
         # Manually defined exposure percentages for each face
-        east_exposure = [1, 1, 1, 0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0, 0, 0, 0]
-        west_exposure = list(reversed(east_exposure))
-        south_exposure = [0, 0, 0, 0.8, 0.9, 1, 1, 1, 0.9, 0.8, 0.7, 0.6, 0.5]
-        north_exposure = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.5, 0.4, 0.3, 0.2, 0, 0, 0]
+        # east_exposure = [1, 1, 1, 0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0, 0, 0, 0]
+        # west_exposure = list(reversed(east_exposure))
+        # south_exposure = [0, 0, 0, 0.8, 0.9, 1, 1, 1, 0.9, 0.8, 0.7, 0.6, 0.5]
+        # north_exposure = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.5, 0.4, 0.3, 0.2, 0, 0, 0]
 
-        exposures = [east_exposure, south_exposure, west_exposure, north_exposure]
+        # exposures = [east_exposure, south_exposure, west_exposure, north_exposure]
+
+        east_exposure_1 = [1, 1, 0.9, 0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0, 0, 0, 0]
+        west_exposure_1 = list(reversed(east_exposure))
+        south_exposure_1 = [0, 0, 0, 0.8, 0.9, 1, 1, 1, 0.9, 0.8, 0.7, 0.6, 0.5]
+        north_exposure_1 = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.5, 0.4, 0.3, 0.2, 0, 0, 0]
+
+        east_exposure_2 = [1, 0.9, 0.8, 0.8, 0.7, 0.5, 0.2, 0.3, 0.2, 0.1, 0, 0, 0, 0]
+        west_exposure_2 = list(reversed(east_exposure))
+        south_exposure_2 = [0, 0, 0, 0.6, 0.7, 0.7, 0.6, 0.6, 0.5, 0.5, 0.7, 0.6, 0.5]
+        north_exposure_2 = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.3, 0.4, 0.6, 0.2, 0, 0, 0]
+
+        east_exposure_3 = [1, 1, 0.8, 0.5, 0.7, 0.5, 0.3, 0.2, 0.1, 0, 0, 0, 0]
+        west_exposure_3 = list(reversed(east_exposure))
+        south_exposure_3 = [0, 0, 0, 0.8, 0.9, 1, 0.8, 1, 0.9, 0.8, 0.7, 0.6, 0.5]
+        north_exposure_3 = [0.1, 0.2, 0.3, 0.4, 0.5, 0.3, 0.2, 0.4, 0.3, 0.2, 0, 0, 0]
+        random_set_index = random.randint(0, 2)
+        exposures=[]
+        if random_set_index == 0:
+            exposures = [east_exposure_1, south_exposure_1, west_exposure_1, north_exposure_1]
+        elif random_set_index == 1:
+            exposures = [east_exposure_2, south_exposure_2, west_exposure_2, north_exposure_2]
+        else:
+            exposures = [east_exposure_3, south_exposure_3, west_exposure_3, north_exposure_3]
 
         for hour in range(6, 19):  # 6 AM to 6 PM
             hour_index = hour - 6
@@ -382,10 +405,10 @@ class ShadowEachFaceView(APIView):
 
             west_shadow = [100, 80, 70 , 80, 70, 50, 30, 20, 10, 0, 0, 0, 0]
             response_data = {
-                "west_shadow" : [100, 80, 70 , 80, 70, 50, 30, 20, 10, 0, 0, 0, 0],
-                "east_shadow ": list(reversed(west_shadow)),
-                "south_shadow" : [50, 60, 70, 60, 90, 100, 100, 100, 90, 80, 0, 0, 0],
-                "north_shadow" : [0, 0, 0, 20, 20, 30, 30, 40, 40, 50, 50, 60, 10]
+                "face1" : [100, 80, 70 , 80, 70, 50, 30, 20, 10, 0, 0, 0, 0],
+                "face2" : [50, 60, 70, 60, 90, 100, 100, 100, 90, 80, 0, 0, 0],
+                "face3": list(reversed(west_shadow)),
+                "face4" : [0, 0, 0, 20, 20, 30, 30, 40, 40, 50, 50, 60, 10]
             }
 
             return Response(response_data, status=status.HTTP_200_OK)
